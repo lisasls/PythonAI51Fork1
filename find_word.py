88565236a0f8516c3add61cfed4989324651fd78
Yelyzaveta Slesarchuk <liezavetta66@gmail.com>
2025-04-13 14:03:27 +0200

    main def

--- find_word.py
+++ find_word.py
@@ -2,3 +2,12 @@
 
 def find_longest():
     longest_word = ''
+
+    with open('C:\\local_new\\input.txt', 'r', encoding='utf-8') as file:
+        data = file.readlines()
+
+        for line in data:
+            if len(line) > len(longest_word):
+                longest_word=line
+
+    return longest_word
